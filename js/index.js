6573b20900cf0c087facc0f85948bd0670aa6b34
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-09-15 22:43:47 -0500

    grafica con datos json

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
-fetch('data.json')
-.then(res => console.log(res))
+
+
+
+fetch('https://apuntesnavarrete.github.io/expenses-chart-component-main/js/data.json')
+.then(res => res.json())
+.then(data => {
+
+    console.log(data)
+
+
+    let day = document.getElementsByClassName("description-chart")
+    let draw = document.getElementsByClassName("draw-chart")
+   
+
+    console.log(data.length)
+
+    for (let number = 0; number <= data.length; number++) {
+        day[number].innerHTML = data[number].day
+        draw[number].style.height = `${data[number].amount/10}em`;
+      }
+})
+
